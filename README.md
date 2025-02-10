963a673d17d6b846b2267fe6203ad59ff2a02df2
Abilon Dias <abilon.amd@gmail.com>
2025-02-10 15:41:07 +0200

    feat: add env vars config for PDF Generator API

--- README.md
+++ README.md
@@ -12,6 +12,9 @@ Create certificates in PDF with the PDF Generator API.
 For running locally, rename a copy `.env.local.example` to `.env.local`, and assign the following variables:
 
 - **CERTIFICATES_PORT**: port for the server to listen on
+- **PDF_GENERATOR_WORKSPACE_ID**: PDF Generator API - workspace identifier
+- **PDF_GENERATOR_API_KEY**: PDF Generator API - API Key
+- **PDF_GENERATOR_API_SECRET**: PDF Generator API - API Secret
 
 ## Running locally
 
--- src/server/config.ts
+++ src/server/config.ts
@@ -3,6 +3,9 @@ import envalid from "envalid"
 
 type Config = {
   CERTIFICATES_PORT: number
+  PDF_GENERATOR_WORKSPACE_ID: string
+  PDF_GENERATOR_API_KEY: string
+  PDF_GENERATOR_API_SECRET: string
 }
 
 const loadConfig = (): Config => {
@@ -12,10 +15,18 @@ const loadConfig = (): Config => {
 
   const env = envalid.cleanEnv(process.env, {
     CERTIFICATES_PORT: envalid.num(),
+
+    PDF_GENERATOR_WORKSPACE_ID: envalid.str(),
+    PDF_GENERATOR_API_KEY: envalid.str(),
+    PDF_GENERATOR_API_SECRET: envalid.str(),
   })
 
   return {
     CERTIFICATES_PORT: env.CERTIFICATES_PORT,
+
+    PDF_GENERATOR_WORKSPACE_ID: env.PDF_GENERATOR_WORKSPACE_ID,
+    PDF_GENERATOR_API_KEY: env.PDF_GENERATOR_API_KEY,
+    PDF_GENERATOR_API_SECRET: env.PDF_GENERATOR_API_SECRET,
   }
 }
 
